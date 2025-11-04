7f818b797483ec9b7daafba61b3a6134fbb6df78
wangyl-ux <2562767950@qq.com>
2025-11-04 11:31:26 +0800

    Update repair.component.ts

--- src/app/components/repair/repair.component.ts
+++ src/app/components/repair/repair.component.ts
@@ -1,17 +1,70 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, numberAttribute } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatListModule } from '@angular/material/list';
 
 @Component({
   selector: 'app-repair',
   templateUrl: './repair.component.html',
+  imports: [
+    CommonModule,       // *ngIf, *ngFor
+    FormsModule,        // [(ngModel)]
+    MatCardModule,      // <mat-card>
+    MatButtonModule,    // <button mat-raised-button>
+    MatListModule,      // <mat-list> / <mat-list-item>
+    MatFormFieldModule, // <mat-form-field>
+    MatInputModule      // <textarea matInput>
+  ]
 })
 export class RepairComponent {
-  patchSuggestion = `
-  修改第22行：加入 nonReentrant 修饰符；
-  修改第45行：引入 SafeMath 库；
-  `;
+
+  code: string = ''; // 合约代码
+  report: any = null; // 用于 *ngIf="report"
+  reports: string = ''; // 审计报告
+
+  patchSuggestions :any = '';
   repairedCode = '';
 
+
+  
+  applySuggestion(){
+    this.patchSuggestions = {
+      suggestion: [
+        { num: '1', line: '22', sug: '加入nonReentrant修饰符；' },
+        { num: '2', line: '45', sug: '引入SafeMath库；' },
+      ]
+    };
+
+  }
+
   applyPatch() {
-    this.repairedCode = '✅ 已生成修复后的智能合约代码（模拟）';
+    this.repairedCode = `
+    pragma solidity ^0.4.16;
+    contract Example{
+      mapping(address => uint) credit;
+      uint sum = 1000;
+      mapping(address => uint) creditl;
+      uint sum1;
+      mapping(address => bool) flag;
+      function withdraw() public {
+        if(flag[msg.sender] == false){
+          credit1[msg.sender] = credit[msg.sender];
+          suml=sum;
+          flag[msg.sender] = true;
+        }
+        if(credit1[msg.sender]>0 && sum1 >= creditl[msg.sender]){
+          creditl[msg.sender]= 0;
+          sum1 -= creditl[msg.sender];
+        }else{
+          credit[msg.sender] = credit1[msg.sender];
+          sum = sum1;
+          requier(false);}
+      }
+    }`;
   }
 }
+
