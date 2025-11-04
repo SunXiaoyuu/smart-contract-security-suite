515cbc62457ac33b493baa7a83a150ff0e956810
xy <1851020987@qq.com>
2025-11-04 10:35:22 +0800

    没辣么丑了

--- src/app/components/workflow/workflow.component.ts
+++ src/app/components/workflow/workflow.component.ts
@@ -1,19 +1,25 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { MatCardModule } from '@angular/material/card';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
-import { MatButtonModule } from '@angular/material/button';
 import { CommonModule } from '@angular/common';
-import { MatListModule } from '@angular/material/list';
 import { RouterModule } from '@angular/router';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+
 @Component({
   selector: 'app-workflow',
-  standalone: true,  // ✅ 如果你的项目是standalone结构，这行必须保留
-  imports: [CommonModule, MatCardModule,  MatFormFieldModule,MatInputModule,FormsModule,MatButtonModule,MatListModule,RouterModule ],
+  standalone: true,
+  imports: [
+    CommonModule,
+    RouterModule,
+    FormsModule,
+    MatCardModule,
+    MatButtonModule,
+    MatIconModule,
+    MatListModule,
+  ],
   templateUrl: './workflow.component.html',
   styleUrl: './workflow.component.css'
 })
-export class WorkflowComponent {
-
-}
+export class WorkflowComponent {}
